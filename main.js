47fe6f53195887da1f321c736f38386ef6ca6612
Ryan Hansberry <rhansby@gmail.com>
2015-04-04 18:13:18 -0700

    Added camera controls based on the x,y position of the hand.

--- main.js
+++ main.js
@@ -3,13 +3,27 @@ var robot = require("robotjs");
 
 var bangEventEmitted = false;
 
-function clamp(val, min, max) {
-    return Math.min(Math.max(val, min), max);
+//Move value towards the target by the specfied amount
+function moveTowards(val, target, amount) {
+    var dir = (val > target) ? -1 : 1;
+
+    return val + (amount * dir);
 }
 
-var sensitivity = 0.08;
-var xTolerance = 40;
+//Camera benhavior variables
+var sensitivityX = 0.09;
+var sensitivityY = 0.0725;
 
+var centerX = 0;
+var x_box_width = 12.5;
+
+var centerY = 220;
+var y_box_width = 15;
+
+var x_safe = [centerX - x_box_width, centerX + x_box_width]
+var y_safe = [centerY - y_box_width, centerY + y_box_width];
+
+//Main event loop
 var controller = new Leap.Controller()
 controller.setBackground(true);
 controller.loop(function(frame) {
@@ -20,30 +34,38 @@ controller.loop(function(frame) {
     var mouse = robot.getMousePos();
     frame.hands.forEach(function(hand) {
 
+        //Update camera position
         var posX = hand.indexFinger.stabilizedTipPosition[0];
-        var handDirectionY = hand.direction[1];
+        var posY = hand.indexFinger.stabilizedTipPosition[1];
 
-        // This is kasey stuff:
-        /*
-        var yVec = Leap.vec3.fromValues(0, handDirectionY, 0);
-        var yBasis = Leap.vec3.fromValues(0,1,0);
-        var yAngle = Math.asin(Leap.vec3.dot(yVec, yBasis) / handDirectionY);
-        console.log(Leap.vec3.dot(yVec, yBasis) / handDirectionY);
-        var angleLimit = (7*Math.PI)/18;
-        var yAngle = clamp(yAngle, -angleLimit, angleLimit);
-        var newY = 799 + ((yAngle + angleLimit) / (2*angleLimit));
-        robot.moveMouse(mouse.x + (posX * sensitivity), newY);
-        */
-
-
-
-        var yComponentThreshold = 0.5;
-        if (handDirectionY > yComponentThreshold) {
-            robot.moveMouse(mouse.x + (posX * sensitivity), mouse.y - 5);
-        } else {
-            robot.moveMouse(mouse.x + (posX * sensitivity), (-handDirectionY * mouse.y * 2) + 400);
+        if (posX > x_safe[0] && posX < x_safe[1]) {
+            posX = 0;
+        }
+        else {
+            posX = moveTowards(posX, centerX, x_box_width);
         }
 
+        //Disable y mostion if we're in the safe box.
+        if (posY > y_safe[0] && posY < y_safe[1]) {
+            posY = centerY;
+        }
+        else {
+            posY = moveTowards(posY, centerY, y_box_width);
+
+            //Add a scaling factor on downwards motion to compensate for reduced range of motion
+            if (posY < centerY) {
+                var min_scale = 2;
+                var max_scale = 4.5;
+                var y_max = centerY - y_box_width;
+                var scale = min_scale + (max_scale - min_scale) * (posY - y_max)/(-y_max);
+                posY *= 1/scale;
+            }
+        }
+
+        robot.moveMouse(mouse.x + (posX * sensitivityX), mouse.y + ((centerY - posY) * sensitivityY));
+
+
+        //Check for a trigger pull gesture
         if (!hand.indexFinger.extended || !hand.middleFinger.extended) {
             if (!bangEventEmitted) {
                 console.log('BANG!!!');
